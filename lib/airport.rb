14932cda86b464ab2eecf217c423ab1cd5c5c05a
Robin <robindoble@gmai.com>
2014-04-21 20:00:27 +0100

    airport class can now house multiple plane objects and release them (planes taking off)

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,16 +1,27 @@
 class Airport
 
+# def initialize 
+# 	@planes = []
+# end
+
+def planes
+	@planes ||= [] 
+end
 
 def has_planes?
-true
+	!planes.any?
 end
 
 def land(plane)
-
+	planes << plane
 end
 
 def plane_count
-2
+	planes.count
+end
+
+def take_off(plane)
+	planes.delete(plane)
 end
 
 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -11,11 +11,15 @@ let (:plane) {double :plane}
 	end
 
 	it 'can land more than one plane' do
-		airport.land(plane)
+		2.times {airport.land(plane)}
 		expect(airport.plane_count).to eq(2)
 	end
 
-	
+	it 'planes can take off' do
+		airport.land(plane)
+		airport.take_off(plane)
+		expect(airport.plane_count).to be(0)
+	end
 
 	
 end
